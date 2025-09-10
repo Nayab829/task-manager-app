62dff09c63ef123f98d01b1af4a31ab5a3613e34
Nayab829 <nayabg829@gmail.com>
2025-09-10 21:53:56 +0500

    Update apiPaths.js

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://task-manager-backend-yv4f.onrender.com"
 export const API_PATHS = {
     AUTH: {
         REGISTER: "/api/users/register",
